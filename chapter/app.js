a35bbcd6fe68a73725acb3d58cecbc7b42c6bfe7
muhammadghous6612 <154689572+muhammadghous6612@users.noreply.github.com>
2024-02-29 16:08:22 +0500

    adding chapter 1

--- chapter/app.js
+++ chapter/app.js
@@ -1,13 +1,11 @@
 
-var num1=-3.5
-var num2=3
+var num1='GHOUS'
+var num2=TRUE
 var num3=8
-var ab=(num1**num2)-num3
+// var ab=(num1**num2)-num3
 
-var num4=3
-var num5=8
-var ad=(num4**3)-8
-var gg=(4/3*3.141*1.9**3)-(4/3*3.141*2**3)
 
 // var gr=(2.5-3)
-console.log(gg)
+alert(num1)
+alert(num2)
+alert(num3)
