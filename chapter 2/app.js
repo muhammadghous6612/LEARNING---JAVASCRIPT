a6d949573d2ddfc6dc0410b783e11b3f7a50f320
muhammadghous6612 <154689572+muhammadghous6612@users.noreply.github.com>
2024-02-29 15:50:43 +0500

    aadding and deleting in this file

--- chapter 2/app.js
+++ chapter 2/app.js
@@ -1,7 +1,106 @@
 
-var firstName="Ghous"
-var Age=19
-var reSult=true
-alert(firstName)
-alert(Age)
-alert (reSult)
+// // var firstName="Ghous"
+// // var Age=19
+// // var reSult=true
+// // alert(firstName)
+// // alert(Age)
+// // alert (reSult)
+// console.log(10+2-3*4)
+
+// var num1=20
+// var num2=60
+
+// var addition=num1+num2
+// var num3=30
+// var num4=80
+
+// var modulas=num3%num4
+// console.log(modulas)
+// // 80 1200 3 40 0
+// var add=num1+num2
+// var malt=num1*num2
+// var div=num2/num1
+// var sub=num1-num2
+// var mod=num1%num2
+// var bodmas1=(add+malt)
+// console.log(bodmas1)
+
+
+// 1. Given var x = 25 and var y = 7, what is the result of x % y?
+var x=25
+var y=7 
+var modulas=x%y
+console.log(modulas)
+
+// 2. If var a = 15 and var b = 4, what is the value of a * b after adding 10 to a and subtracting 2 from b?
+var a =15
+var b=4
+var ab=(a*b)
+
+// console.log(ab)
+// 3. Suppose var c = 12 and var d = 3, calculate the result of c ** d.
+var c=12
+var d=3
+var cd=(c**d)
+console.log(cd)
+// 4. For var e = 10 and var f = 3, what is the value of e + f * 2?
+var e=10
+var f=3
+var ef=(e+f*2)
+
+console.log(ef)
+// 5. Considering var g = 20 and var h = 4, what will be the outcome of g / h after adding 5 to g?
+var g=20
+var h=4
+var gh=(g/h)+5
+// console.log(gh)
+
+// 6. Given var i = 8 and var j = 3, determine the value of i * j after subtracting 2 from i.
+var i=8
+var j=3
+var ij=(i*j)
+// console.log(ij)
+// 7. If var k = 18 and var l = 5, what is the result of k - l + 2?
+var k=18
+var l=5
+var kl2=(k-l+2)
+console.log(kl2)
+// 8. Suppose var m = 7 and var n = 2, calculate the value of m ** n.
+var m=7
+var n=2
+var mn=(m**n)
+console.log(mn)
+// 9. For var o = 13 and var p = 4, what will be the outcome of o % (p + 2)?
+var o=13
+var p=4
+var op=o%(p+2)
+console.log(op)
+// 10. Given var q = 16 and var r = 3, determine the result of q * r after dividing q by 2.
+var q=16
+var r=3
+// var qr=(q*r)(q/2)
+// console.log(qr)
+// 11. If var s = 25 and var t = 3, what is the value of s / t after subtracting 1 from s?
+var s=25
+var t=3
+// var st=(s-1/t)
+// 12. Suppose var u = 11 and var v = 3, calculate the value of u - v after adding 2 to v.
+var u=11
+var v=3
+// var uv=(u-v)*v+2
+// console.log(uv)
+// 13. For var w = 22 and var x = 5, what will be the outcome of w + (x ** 2)?
+var w=22
+var x=5
+var wx=w+(x**2)
+console.log(wx)
+// 14. Given var y = 19 and var z = 4, determine the result of y % (z + 1).
+var y=19
+var z=4
+var yz=y%(z+1)
+console.log(yz)
+// 15. If var a = 10 and var b = 3, what is the value of a * (b - 2)?
+var a=10
+var b=3
+var aba=a*(b-2)
+console.log(aba)
--- chapter/app.js
+++ chapter/app.js
@@ -1,6 +1,13 @@
 
+var num1=-3.5
+var num2=3
+var num3=8
+var ab=(num1**num2)-num3
 
-var firstName="Ghous"
-var Age=19
-alert(firstName)
-alert(Age)
+var num4=3
+var num5=8
+var ad=(num4**3)-8
+var gg=(4/3*3.141*1.9**3)-(4/3*3.141*2**3)
+
+// var gr=(2.5-3)
+console.log(gg)
